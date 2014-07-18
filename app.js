4dbc545e9f226cd8ca4ffa2f961f9310bdced041
wcm381 <wcm381@gmail.com>
2014-07-18 14:30:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,60 +23,56 @@ var connection = mysql.createConnection({
 });
 
 
-// Connect to SQL Database
-connection.connect(function(err) {
-if (err) {
-   console.log("ERROR: " + err.message);
-   throw err;
-}
-});
-
-
-// Create Table
-connection.query( 'CREATE TABLE IF NOT EXISTS sampleTbl (' +
-  'P_Id INT NOT NULL AUTO_INCREMENT,' +
-  'Time DATETIME NOT NULL,' +
-  'Temp FLOAT NOT NULL,' +
-  'PRIMARY KEY(P_Id)'	+
-  ')', function( err ){
-     if ( err ) {
-      console.log("ERROR: " + err.message);	
-      throw err;
-     }
-     console.log( 'Table created.' );
-});
-
-
-//
+// Create HTTP Server for Arduino Connection
 server = http.createServer( function(req,res){
-   // 
+   // Get URL Path of Request
    var path = url.parse(req.url).pathname;
-   //
+   //Get Current Time; the Approximate Time of POST Request
    var timeVal = getDataTime();
-   
-   //
+   // Check For Correct URL Path
    switch (path){
     case '/':
-      //
+      // Check for Proper Request Method
       if (req.method == 'POST') {
-        //
+        // Connect to SQL Database
+        connection.connect(function(err) {
+        if (err) {
+           console.log("ERROR: " + err.message);
+           throw err;
+        }
+        });
+        // Create Table
+        connection.query( 'CREATE TABLE IF NOT EXISTS sampleTbl (' +
+          'P_Id INT NOT NULL AUTO_INCREMENT,' +
+          'Time DATETIME NOT NULL,' +
+          'Temp FLOAT NOT NULL,' +
+          'PRIMARY KEY(P_Id)'	+
+          ')', function( err ){
+             if ( err ) {
+              console.log("ERROR: " + err.message);	
+              throw err;
+             }
+             console.log( 'Table created.' );
+        });
+        // Acknowledge POST Request Method in Console
         console.log("POST");
-        //  
+        //  Initialize Variable to Store POST Data 
         var body = "";
-        //
+        // If Data and No 'end' Received
         req.on('data', function(data) {
             body += data;
         });
-        //
+        // Received 'end' of POST Request
         req.on('end', function () {
+            // Display Received Request Data
             console.log("Body: " + body);
-            // 
+            // Pasre JSON Packet Received From Arduino
             var obj = JSON.parse(body);
-            
-            
-            //
-            var dataSet = { Time: timeVal, 
-            //
+            // Get Components of Parsed JSON Packet
+            var tempVal = obj.temp;
+            // Create SET to Insert Into Table
+            var dataSet = { Time: timeVal, Temperature: tempVal };
+            // Insert the SET to Table
             connection.query( 'INSERT INTO sampleTbl SET ?', sampleSet, function(err, result) {
                 if ( err ) {
                     console.log("ERROR: " + err.message);	
@@ -84,11 +80,7 @@ server = http.createServer( function(req,res){
                 }
                 console.log(result);
             });
-            
-            
             //
-            
-            
             // DEBUGGING: Check Table Rows
             connection.query('SELECT * FROM sampleTbl', function (err, rows, result) {
                 if ( err ) throw err;
@@ -96,17 +88,8 @@ server = http.createServer( function(req,res){
                         console.log('Temp Values: ', rows[i].Temp);	
                     }
             });
-            
-            
             // End Connection to Database
-            connection.end();	
-            
-            
-            
-            
-            
-            
-            
+            connection.end();	 
         });
       } else {
         // Should Not Reach Here
@@ -120,17 +103,10 @@ server = http.createServer( function(req,res){
         break;
     }
 });
-  
-  
+// Set Port Number for HTTP Server to Listen To  
 server.listen(HTTP_PORT);
 
-
-
-
-
-
-
-
+// Get the Current Date and Time and Return in MySQL Date Data Type Format
 function getDateTime() {
     var date = new Date();
     var hour = date.getHours();
